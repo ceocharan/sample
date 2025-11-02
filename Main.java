a178ab7899161b67ccc36be8cf338bf10e4df901
Sricharan Peddinti <sricharan_peddinti@epam.com>
2025-11-02 20:38:18 +0530

    Fixed

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
 public class Main {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Main"); 
+        System.out.println("first"); 
+        System.out.println("bye");      
+    }
 }
