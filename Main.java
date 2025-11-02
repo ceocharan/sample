a6aff49cf96b7dbe68c322350c8b15042e753eb9
Sricharan Peddinti <sricharan_peddinti@epam.com>
2025-11-02 20:59:59 +0530

    done

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main"); 
         System.out.println("first"); 
-        System.out.println("bye");      
+        System.out.println("bye"); 
+        System.out.println("sricharna");     
     }
 }
